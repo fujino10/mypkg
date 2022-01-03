01e688830d2c89c43390d7f03a8c5f25d0183e2a
fujino10 <s20c1105py@s.chibakoudai.jp>
2022-01-03 07:22:14 +0000

    fix

--- mypkg/scripts/twice.py
+++ mypkg/scripts/twice.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import rospy
-from std_msds.msg import Init32
+from std_msgs.msg import Int32
 
 def cb(message):
     rospy.loginfo(message.data*2)
 
 
 rospy.init_node('twice')
-sub = rospy.subscribe('count_up', Init32, cb)
+sub = rospy.Subscriber('count_up', Int32, cb)
 rospy.spin() #this line mean not end the progrum. if it do not exist, this progrum would end straight away
 
 
