96014fb5a2aa648af6e35a18e143b2039da4108b
fujino10 <s20c1105py@s.chibakoudai.jp>
2022-01-05 01:35:23 +0000

    modify spell miss

--- mypkg/scripts/prime.py
+++ mypkg/scripts/prime.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import rospy
-from srd_msgs.msg import Int32
+from std_msgs.msg import Int32
+
+global i
 
 i = message.data
 
@@ -17,5 +19,5 @@ rospy.init_node('prime')
 sub = rospy.Subscriber('count_up', Int32, is_prime)
 pub = rospy.Publisher('prime', Int32, queue_size=1)
 while not rospy.is_shutdown():
-    pub.publish('Is number  {0} Prime?.{1}'.format(i,is_prime(i)))
+    pub.publish('Is number {0} Prime?.{1}'.format(i,is_prime(i)))
     rate.sleep()
