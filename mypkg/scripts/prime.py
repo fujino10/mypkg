9623ae3167220456a7ef18fabed7f9b668e40664
fujino10 <s20c1105py@s.chibakoudai.jp>
2022-01-05 01:38:16 +0000

    modify construction

--- mypkg/scripts/prime.py
+++ mypkg/scripts/prime.py
@@ -3,9 +3,11 @@
 import rospy
 from std_msgs.msg import Int32
 
-global i
+i = 0
+def cb(message):
+    global i
+    i = message.data
 
-i = message.data
 
 def is_prime(i):
     if i <= 1:
@@ -16,7 +18,7 @@ def is_prime(i):
         return True
 
 rospy.init_node('prime')
-sub = rospy.Subscriber('count_up', Int32, is_prime)
+sub = rospy.Subscriber('count_up', Int32, cb)
 pub = rospy.Publisher('prime', Int32, queue_size=1)
 while not rospy.is_shutdown():
     pub.publish('Is number {0} Prime?.{1}'.format(i,is_prime(i)))
