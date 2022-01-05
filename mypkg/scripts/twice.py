344f9f1fae9f5b13d2ad650d7046815e1faf7852
fujino10 <s20c1105py@s.chibakoudai.jp>
2022-01-05 01:09:34 +0000

    Inittial commit

--- mypkg/scripts/twice.py
+++ mypkg/scripts/twice.py
@@ -2,12 +2,20 @@
 import rospy
 from std_msgs.msg import Int32
 
+n = 0
+
 def cb(message):
-    rospy.loginfo(message.data*2)
+    global n
+    n = message.data*2
 
 
 rospy.init_node('twice')
 sub = rospy.Subscriber('count_up', Int32, cb)
-rospy.spin() #this line mean not end the progrum. if it do not exist, this progrum would end straight away
+pub = rospy.Publisher('twice', Int32, queue_size=1)
+rate = rospy.Rate(10)
+while not rospy.is_shutdown():
+    pub.publish(n)
+    rate.sleep()
+
 
 
