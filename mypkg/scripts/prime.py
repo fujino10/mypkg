d3d3ec9ebc7d3e9b0155445225144d3d3a4bc540
fujino10 <s20c1105py@s.chibakoudai.jp>
2022-01-05 01:29:54 +0000

    fix

--- mypkg/scripts/prime.py
+++ mypkg/scripts/prime.py
@@ -3,8 +3,9 @@
 import rospy
 from srd_msgs.msg import Int32
 
-def is_prime(message):
-    i = message.data
+i = message.data
+
+def is_prime(i):
     if i <= 1:
         return False
     for j in range(2, int(i ** 0.5) + 1):
@@ -16,5 +17,5 @@ rospy.init_node('prime')
 sub = rospy.Subscriber('count_up', Int32, is_prime)
 pub = rospy.Publisher('prime', Int32, queue_size=1)
 while not rospy.is_shutdown():
-    pub.publish(i is)
+    pub.publish('Is number  {0} Prime?.{1}'.format(i,is_prime(i)))
     rate.sleep()
