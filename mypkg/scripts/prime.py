aa15dc43c8d0364e6d2354ca6ee29f4ce431891a
fujino10 <s20c1105py@s.chibakoudai.jp>
2022-01-05 02:18:05 +0000

    fix

--- mypkg/scripts/prime.py
+++ mypkg/scripts/prime.py
@@ -15,12 +15,14 @@ def is_prime(i):
     for j in range(2, int(i ** 0.5) + 1):
         if i  % j == 0:
             return False
-        return True
+    return True
 
 rospy.init_node('prime')
 sub = rospy.Subscriber('count_up', Int32, cb)
 pub = rospy.Publisher('prime', Int32, queue_size=1)
 rate = rospy.Rate(10)
 while not rospy.is_shutdown():
-    pub.publish('Is number {0} Prime?.{1}'.format(i,is_prime(i)))
+    prime_str = "Is number {0} Prime? {1}".format(i,is_prime(i))
+    rospy.loginfo(prime_str)
+    pub.publish(prime_str)
     rate.sleep()
