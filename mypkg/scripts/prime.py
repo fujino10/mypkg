462fc87a763c18a00ee3711c1ad9a471bd5f3300
fujino10 <s20c1105py@s.chibakoudai.jp>
2022-01-07 02:51:17 +0000

    add new publisher

--- mypkg/scripts/prime.py
+++ mypkg/scripts/prime.py
@@ -4,25 +4,30 @@ import rospy
 from std_msgs.msg import Int32
 
 i = 0
+c = 0
 def cb(message):
     global i
     i = message.data
 
 
 def is_prime(i):
+    global c
     if i <= 1:
         return False
     for j in range(2, int(i ** 0.5) + 1):
         if i  % j == 0:
             return False
+    c += 1
     return True
 
 rospy.init_node('prime')
 sub = rospy.Subscriber('count_up', Int32, cb)
-pub = rospy.Publisher('prime', Int32, queue_size=1)
+pub1 = rospy.Publisher('prime_number', Int32, queue_size=1)
+pub2 = rospy.Publisher('prime_count', Int32,queue_size=1 ) 
 rate = rospy.Rate(10)
 while not rospy.is_shutdown():
     prime_str = "Is number {0} Prime? {1}".format(i,is_prime(i))
     rospy.loginfo(prime_str)
-    pub.publish(prime_str)
+    pub1.publish(prime_str)
+    pub2.publish(c)
     rate.sleep()
