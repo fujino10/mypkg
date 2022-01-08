8485819093008f897df1d18371b2e7b1edc74ed0
fujino10 <s20c1105py@s.chibakoudai.jp>
2022-01-08 05:47:04 +0000

    fix

--- mypkg/scripts/prime.py
+++ mypkg/scripts/prime.py
@@ -5,6 +5,7 @@ from std_msgs.msg import Int32
 
 i = 0
 c = 0
+hoge = 0
 def cb(message):
     global i
     i = message.data
@@ -12,12 +13,14 @@ def cb(message):
 
 def is_prime(i):
     global c
+    global hoge
     if i <= 1:
         return False
     for j in range(2, int(i ** 0.5) + 1):
         if i  % j == 0:
             return False
     c += 1
+    hoge = i
     return True
 
 rospy.init_node('prime')
@@ -28,6 +31,6 @@ rate = rospy.Rate(10)
 while not rospy.is_shutdown():
     prime_str = "Is number {0} Prime? {1}".format(i,is_prime(i))
     rospy.loginfo(prime_str)
-    pub1.publish(prime_str)
+    pub1.publish(hoge)
     pub2.publish(c)
     rate.sleep()
